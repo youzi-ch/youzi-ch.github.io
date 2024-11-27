7e4fcbb27e24def9b262d5ee6e82939a3843325d
youzi-ch <1841783356@qq.com>
2024-11-27 21:18:13 +0800

    Site updated: 2024-11-27 21:18:12

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732707184034";
+const VERSION = "1732713486333";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
